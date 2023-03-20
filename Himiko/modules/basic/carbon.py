2bbdc930b802b72344069e745de01c66d25bf5c0
adam <104802621+d4msy@users.noreply.github.com>
2023-03-20 18:46:58 +0700

    Create carbon.py

---
+++ Himiko/modules/basic/carbon.py
@@ -0,0 +1,58 @@
+
+import asyncio
+from io import BytesIO
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from config import CMD_HANDLER as cmd
+from Himiko import aiosession
+from Himiko.helpers.basic import edit_or_reply
+from Himiko.helpers.PyroHelpers import ReplyCheck
+
+from .help import add_command_help
+
+
+async def make_carbon(code):
+    url = "https://carbonara.vercel.app/api/cook"
+    async with aiosession.post(url, json={"code": code}) as resp:
+        image = BytesIO(await resp.read())
+    image.name = "carbon.png"
+    return image
+
+
+@Client.on_message(filters.command(["carbon"], "") & filters.me)
+async def carbon_func(client: Client, message: Message):
+    text = (
+        message.text.split(None, 1)[1]
+        if len(
+            message.command,
+        )
+        != 1
+        else None
+    )
+    if message.reply_to_message:
+        text = message.reply_to_message.text or message.reply_to_message.caption
+    if not text:
+        return await message.delete()
+    Man = await message.reply("`Processing...`")
+    carbon = await make_carbon(text)
+    await Man.edit("`Uploading...`")
+    await asyncio.gather(
+        Man.delete(),
+        client.send_photo(
+            message.chat.id,
+            carbon,
+            caption=f"**Carbonised by** {client.me.mention}",
+            reply_to_message_id=ReplyCheck(message),
+        ),
+    )
+    carbon.close()
+
+
+add_command_help(
+    "carbon",
+    [
+        ["carbon <reply>", "Carbonize text with default settings."],
+    ],
+)
