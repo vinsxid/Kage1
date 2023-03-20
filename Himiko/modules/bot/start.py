567868272914c5a6744d8b1c6867d21d2e30c4d6
adam <104802621+d4msy@users.noreply.github.com>
2023-03-20 23:09:07 +0700

    Create start.py

---
+++ Himiko/modules/bot/start.py
@@ -0,0 +1,55 @@
+from Himiko import app
+import os
+import time
+import re
+import asyncio
+import math
+import shutil
+import sys
+from pyrogram import Client, enums, filters
+from os import environ, execle, path
+from Himiko import *
+from Himiko.utils.misc import *
+from pyrogram import *
+from platform import python_version as py
+from pyrogram import __version__ as pyro
+from pyrogram.types import * 
+from config import *
+from Himiko import ids
+
+ADMIN1 = [1725671304]
+ADMIN2 = [2049080295]
+APP_VERSION = "7.2.0"
+
+
+@app.on_message(filters.command(["alive"]))
+async def module_help(client: Client, message: Message):
+    served_users = len(ids)
+    await message.reply_text(
+      f"""
+ **HimikoUbot**
+   **Status** : **ROBOT**
+     **Users** : `{served_users}`
+     **Versi** : `{APP_VERSION}`
+     **Python** : `{py()}`
+     **Pyrogram** : `{pyro}`
+""",
+    reply_markup=InlineKeyboardMarkup(
+      [
+        [
+            InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ​", url=f"https://t.me/HimikoSupportChat"),
+            InlineKeyboardButton(text="ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/diarydam"),
+        ],
+      ]
+      ),
+      disable_web_page_preview=True,
+    )
+
+@app.on_message(filters.command(["start"]) & filters.private)
+async def start_(client: Client, message: Message):
+    await message.reply_text(
+        f"""<b>👋 **Hallo {message.from_user.first_name}** \n
+💭 **Is there anything I can help? **
+💡 **If you want to make a bot\nYou can Contaci @d4msy.**
+</b>""",
+    )
