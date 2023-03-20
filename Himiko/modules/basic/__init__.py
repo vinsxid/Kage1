e5eec6cc743a39432d4868b2fd381615953fffe7
adam <104802621+d4msy@users.noreply.github.com>
2023-03-20 17:08:14 +0700

    Update __init__.py

--- Himiko/modules/basic/__init__.py
+++ Himiko/modules/basic/__init__.py
@@ -1,1 +1,15 @@
+import os
+import sys
+from Himiko.modules.basic.help import add_command_help
 
+
+def restart():
+    os.execvp(sys.executable, [sys.executable, "-m", "Himiko"])
+
+async def join(client):
+    try:
+        await client.join_chat("HimikoSupportChat")
+        await client.join_chat("diarydam")
+        await client.join_chat("SharingUserbot")
+    except BaseException:
+        pass
