f5e6c3340a1f50a7adeb25ca3bc4109744de6952
adam <104802621+d4msy@users.noreply.github.com>
2023-03-21 10:52:00 +0700

    Update ask.py

--- Himiko/modules/basic/ask.py
+++ Himiko/modules/basic/ask.py
@@ -13,8 +13,8 @@ from .help import add_command_help
 
 OPENAI_API_KEY = "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"
 
-@Client.on_message(filters.command(["cai"], "") & filters.user(DEVS) & ~filters.me)
-@Client.on_message(filters.command(["ai"], "") & filters.me)
+@Client.on_message(filters.command(["cask"], cmd) & filters.user(DEVS) & ~filters.me)
+@Client.on_message(filters.command(["ai", "ask"], cmd) & filters.me)
 async def openai(client: Client, message: Message):
     if len(message.command) == 1:
         return await message.reply(f"Type <code>.{message.command[0]} [question]</code> Question to use OpenAI")
@@ -39,7 +39,7 @@ async def openai(client: Client, message: Message):
     except Exception:
         await msg.edit("There is an error!!\nYou have not entered OPENAI_API_KEY")
     
-@Client.on_message(filters.command(["img"], "") & filters.me)
+@Client.on_message(filters.command(["img"], cmd) & filters.me)
 async def _(client, message):
     Tm = await message.reply("<code>Processing...</code>")
     if len(message.command) < 2:
@@ -56,7 +56,7 @@ async def _(client, message):
 add_command_help(
     "openai",
     [
-        ["ai [query]",
+        ["ai or ask [query]",
             "Generate or manipulate text"],
         ["img [query]",
             "Generate or manipulate images"],
