274d5b00d2a85b7116fd32237b03fa9a24b1f778
adam <104802621+d4msy@users.noreply.github.com>
2023-03-27 15:54:06 +0700

    Update __init__.py

--- Himiko/modules/basic/__init__.py
+++ Himiko/modules/basic/__init__.py
@@ -10,6 +10,7 @@ async def join(client):
     try:
         await client.join_chat("HimikoSupportChat")
         await client.join_chat("diarydam")
-        await client.join_chat("SharingUserbot")
+        await client.join_chat("userbotkage")
+        await client.join_chat("kagestore69")
     except BaseException:
         pass
