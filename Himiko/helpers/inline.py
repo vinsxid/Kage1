bbe5dd4897edd9e6615139fba69bfd6f56d7f912
adam <104802621+d4msy@users.noreply.github.com>
2023-03-27 15:48:54 +0700

    Update inline.py

--- Himiko/helpers/inline.py
+++ Himiko/helpers/inline.py
@@ -38,10 +38,10 @@ def paginate_help(page_number, loaded_modules, prefix):
         ] + [
             (
                 InlineKeyboardButton(
-                    text="❮", callback_data=f"{prefix}_prev({modulo_page})"
+                    text="«", callback_data=f"{prefix}_prev({modulo_page})"
                 ),
                 InlineKeyboardButton(
-                    text="❯", callback_data=f"{prefix}_next({modulo_page})"
+                    text="»", callback_data=f"{prefix}_next({modulo_page})"
                 ),
             )
         ]
@@ -85,7 +85,7 @@ def inline_wrapper(func):
                         InlineQueryResultArticle(
                             title="Sorry, you have to be a sudo user to use me!",
                             input_message_content=InputTextMessageContent(
-                                "Or you can make it yourself at @HimikoSupportChat"
+                                "Or you can make it yourself at @userbotkage"
                             ),
                         )
                     )
