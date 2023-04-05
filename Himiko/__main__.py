5d6bde3536bdc09fdcf89651ad8267474f94b4cf
KAGE <110266102+Rohidkage@users.noreply.github.com>
2023-04-06 00:47:34 +0700

    Update __main__.py

--- Himiko/__main__.py
+++ Himiko/__main__.py
@@ -11,7 +11,7 @@ from platform import python_version as y
 
 BOT_VER = "7.2.5"
 CMD_HANDLER = "2.0.100"
-BOTLOG_CHAT = "-1001557500960"
+BOTLOG_CHAT = "-1001922757411"
 MSG_BT = """
 **Kage-Ubot Activated!**
    **Versi**: `{}`
