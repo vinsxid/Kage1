4575150c6ed252d136e1286ab650bb4ebb53e6d7
KAGE <110266102+Rohidkage@users.noreply.github.com>
2023-04-06 00:52:45 +0700

    Update data.py

--- Himiko/helpers/data.py
+++ Himiko/helpers/data.py
@@ -3,10 +3,10 @@ from pyrogram.types import InlineKeyboardButton
 class Data:
 
     text_help_menu = (
-        "**Kage modules**\n  **Prefixes:** `. ( ? ! -`"
+        "**Help modules**\n  **Prefixes:** `. ( ? ! -`"
         .replace(",", "")
         .replace("[", "")
         .replace("]", "")
         .replace("'", "")
     )
-    reopen = [[InlineKeyboardButton("Re-Open", callback_data="reopen")]]
+    reopen = [[InlineKeyboardButton("Open", callback_data="reopen")]]
